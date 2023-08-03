930440cab5fdcb8c63b71d7bdf830e2083c40f26
Charles.K.Muia@gmail.com <Charles.K.Muia@gmail.com>
2023-08-03 13:01:31 +0300

    Responsive_Restaurant_Build

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BwK Restaurant | Home</title>
+
+<!--===GOOGLE FONTS---------------------------------------------------------------------------------------------------->
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Righteous&family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100&display=swap" rel="stylesheet">
+
+<!--===Favicon Icon---------------------------------------------------------------------------------------------------->
+
+    <link rel="icon" href="Gallery/Logo2.jpg">
+
+<!--===CSS Style Sheets------------------------------------------------------------------------------------------------>
+
+    <link rel="stylesheet" href="style.css">
+
+<!--===CDN------------------------------------------------------------------------------------------------------------->
+
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+</head>
+<body>
+
+</body>
+</html>
