e09a5c0fc02401c31dfe75ce08f4c92bae0bcce3
Charles.K.Muia@gmail.com <Charles.K.Muia@gmail.com>
2023-08-06 15:39:23 +0300

    Responsive_Restaurant_Build

--- index.php
+++ index.php
@@ -32,6 +32,7 @@
 <!--===CDN------------------------------------------------------------------------------------------------------------->
 
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 
 </head>
 <body>
@@ -45,6 +46,10 @@
                         <a href="#"><img src="Gallery/Resized_lg_3.png" alt=""></a>
                     </div>
                         <nav>
+                                <div class="btn">
+                                    <i class="fas fa-times close-btn"></i>
+                                </div>
+
                             <li><a href="#">home</a></li>
                             <li><a href="#">about</a></li>
                             <li><a href="#">menu</a></li>
@@ -52,6 +57,9 @@
                             <li><a href="#">blog</a></li>
                             <li><a href="#">contact</a></li>
                         </nav>
+                                <div class="btn">
+                                    <i class="fas fa-bars menu-btn"></i>
+                                </div>
                 </div>
             </div>
         </header>
@@ -64,10 +72,91 @@
                     <img src="Gallery/Resized_lg_3.png" alt="">
                         <h2> — fine dining at its best — </h2>
                             <h3> a genuine experience awaits </h3>
-                    
                 </div>
             </div>
         </section>
+
+<!----===OFFERS SECTION-------------------------------------------------------------------------------------------------->
+
+        <section class="offers-sec">
+            <aside class="offers-side">
+                <div class="container">
+                    <div class="offers-side-content">
+                        <div class="left-side-content">
+                            <div class="lsc-heading">
+                                <h3>finger licking menu</h3>
+                                    <h2>master-chef orders</h2>
+                            </div>
+                            <div class="img-content">
+                                <div class="images">
+                                    <img src="Gallery/Kienyeji.jpg" alt="">
+                                    <div class="img-content">
+                                        <h4>kienyeji, greens & ugali</h4>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque laborum natus quae quod voluptatem? Cumque perferendis perspiciatis praesentium repudiandae ut.</p>
+                                    </div>
+                                </div>
+                                <div class="prices">
+                                    <p>kes 450 </p>
+                                </div>
+                            </div>
+
+                            <div class="img-content">
+                                <div class="images">
+                                    <img src="Gallery/Salad.jpg" alt="">
+                                    <div class="img-content">
+                                        <h4>kienyeji, greens & ugali</h4>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque laborum natus quae quod voluptatem? Cumque perferendis perspiciatis praesentium repudiandae ut.</p>
+                                    </div>
+                                </div>
+                                <div class="prices">
+                                    <p>kes 450 </p>
+                                </div>
+                            </div>
+
+                            <div class="img-content">
+                                <div class="images">
+                                    <img src="Gallery/Meat.jpg" alt="">
+                                    <div class="img-content">
+                                        <h4>kienyeji, greens & ugali</h4>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque laborum natus quae quod voluptatem? Cumque perferendis perspiciatis praesentium repudiandae ut.</p>
+                                    </div>
+                                </div>
+                                <div class="prices">
+                                    <p>kes 450 </p>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="right-side-content">
+                            <div class="img-right-side">
+                                <img src="Gallery/Fish.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </aside>
+        </section>
+
+
+
+
+<!----===JS QUERY SECTION---------------------------------------------------------------------------------------------->
+
+<!----===JS NAV MENU TOGGLE ------------------------------------------------------------------------------------------->
+
+     <script>
+            let menu = document.querySelector("nav");
+            let menuBTN = document.querySelector(".menu-btn");
+            let closeBTN = document.querySelector(".close-btn");
+
+            menuBTN.addEventListener("click", function () {
+                menu.classList.add("active");
+            })
+
+            closeBTN.addEventListener("click", function () {
+                menu.classList.remove("active");
+            })
+     </script>
 
 </body>
 </html>
